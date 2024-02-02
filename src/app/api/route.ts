3232a655f6bb50a4ab1b964bb571980b2fd353ef
bethhampshire <beth-hampshire94@hotmail.co.uk>
2024-02-02 22:25:21 +0000

    Move lib

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,6 +1,6 @@
 import { sql } from '@vercel/postgres';
 import { NextResponse } from 'next/server';
-import  prisma from "@../../../lib/prisma"
+import  prisma from "@/lib/prisma"
 
 // A `main` function so that we can use async/await
 export async function getUsers() {
