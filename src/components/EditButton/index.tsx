fd4c21fa66eaa06f3f220cbca97896c8be8dec07
bethhampshire <beth-hampshire94@hotmail.co.uk>
2024-02-07 18:43:19 +0000

    Add edit props and set up edit mode

--- src/components/EditButton/index.tsx
+++ src/components/EditButton/index.tsx
@@ -1,23 +1,25 @@
 "use client";
 
-import Link from "next/link";
-import styles from "./edit-button.module.css";
-import Image from 'next/image';
-import logo from "../../../public/ft-logo.svg";
-import { useState } from "react";
+interface EditButtonProps {
+    isEditing: boolean,
+    setEdit: any
+}
 
-export default function EditButton(props: any) {
-    const [edit, setEdit] = useState(false);
+export default function EditButton(props: EditButtonProps) {
+    const handleChange = () => {
+            props.setEdit(!props.isEditing);
+        }
+
     return (
     <div>
-        {!edit ? 
-        <button onClick={() => setEdit(!edit)}>
+        {!props.isEditing ? 
+        <button onClick={handleChange}>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                 <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125" />
             </svg>
         </button> : 
         <div>
-            <button onClick={() => setEdit(!edit)}>
+            <button onClick={handleChange}>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 12.75 6 6 9-13.5" />
                 </svg>
--- src/components/Tile/index.tsx
+++ src/components/Tile/index.tsx
@@ -1,21 +1,29 @@
 "use client";
 
 import Link from "next/link";
-import styles from "./tile.module.css";
-import Image from 'next/image';
 import EditButton from "../EditButton";
 import LockButton from "../LockButton";
+import { useState } from "react";
 
 export default function Tile(props: any) {
+    const [edit, setEdit] = useState(false);
+    const onChange = () => {
+        setEdit(!edit)
+      };
+
     return (
     <div className= "{styles.container} shadow-md shadow-slate-800 rounded border-slate-700 border-2 hover:bg-slate-800">
-        <div>
+        {edit ?
+        <p>in edit mode</p>
+         :
+         <div>
         <Link href={`/members/${props.id}`}>
             <h2>{props.firstName}</h2>
             <h3>{props.surname}</h3>
         </Link>
-        </div>
-        <EditButton />
+        </div>}
+        
+        <EditButton isEditing={edit} setEdit={setEdit}/>
         <LockButton />
     </div>
     )
